7a8324311f4a45bdffe3f4434a96fc61255a4ace
Arjunan M. R. || Diode ;) <arjunanmr101@gmail.com>
2020-04-01 08:55:43 +0530

    added geolocation API

--- public/index.js
+++ public/index.js
@@ -10,5 +10,19 @@ const options = {
   },
   body: JSON.stringify(data)
 };
-console.log(data);
-fetch("/diode", options);
+
+fetch("/diode", options)
+  .then(() => console.log("Succesfully send data!"))
+  .catch(error => console.error(error));
+
+const location_options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+navigator.geolocation.getCurrentPosition(
+  pos => console.log(pos),
+  error => console.error(error),
+  location_options
+);
