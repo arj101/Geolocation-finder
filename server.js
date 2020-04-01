1e647adec64adee73f13f3b186e98d65d8295a1e
Arjunan M. R. || Diode ;) <arjunanmr101@gmail.com>
2020-04-01 17:15:03 +0530

    Some finishing touches ;)

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Starting server at ${port} `);
+  console.log(`Started server at ${port}`);
 });
 
 app.use(express.static("public"));
 app.use(express.json());
 
 app.post("/diode", (request, response) => {
-  console.log("I got a request!");
+  console.log("I got a location!");
   const data = request.body;
   console.log(data);
   response.json({
