a452d6fd4fb2029941505ae6527d5738ee14b954
Arjunan M. R. || Diode ;) <arjunanmr101@gmail.com>
2020-04-01 19:55:28 +0530

    added NeDB database!

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const Datastore = require("nedb");
+
 const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
@@ -8,9 +10,13 @@ app.listen(port, () => {
 app.use(express.static("public"));
 app.use(express.json());
 
+const database = new Datastore("database.db");
+database.loadDatabase();
+
 app.post("/diode", (request, response) => {
   console.log("I got a location!");
   const data = request.body;
+  database.insert(data);
   console.log(data);
   response.json({
     status: "success",
